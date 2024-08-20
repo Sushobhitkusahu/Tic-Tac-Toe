e32d19e19ba5290b48bb4be6c3ffcc40eca84951
Sushobhitkusahu <96657127+Sushobhitkusahu@users.noreply.github.com>
2024-08-20 21:04:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// console.log("Sushobhit Sahu");
+
+// let name="5";
+
+// if(name=="x"){
+//     console.log("Sushobhit");
+// }
+// else{
+//     console.log("Sahu");
+// }
+
+// for(let i=1;i < 13; i++)
+// {
+//         console.log("i =", i);
+// }
+
+//for of loop
+
+// let str="Javascript"
+
+// let size=0;
+
+// for(let val of str)
+// {
+//     console.log("val=",val);
+//     size++;
+// }
+
+// console.log("size=",size);
+
+/************************************************************************** */
+//print even numbers from 1 to 100
+
+// for(let i=1;i<=100;i++)
+// {
+//     if(i%2==0)
+//     {
+//         console.log("i=",i);
+//     }
+// }
+    
+
+let marks=[98,87,89,66,45,69];
